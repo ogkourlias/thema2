301777f5d07be9e157947bf9e19eec0bfdb22bc1
Marcel Kempenaar <marcelkempenaar@gmail.com>
2016-11-09 10:54:11 +0100

    Replaced the ATP to ADP module

--- atp_to_adp.py
+++ atp_to_adp.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
 
 '''
-Simple template rendering a molecule originating from PDB files.
+Simple template rendering an ATP molecule originating from a PDB file,
+splitting a phosphate (Pi) group resulting in ADP + Pi.
 
-The animation demonstrates the molecule both rotating and traversing on a
-trajectory using the move_to and rotate methods.
-
-NOTE: also shows how to do prototyping using multithreading; see the
-prototype.ini configuration file.
+Note: the resulting ADP molecule is not in a realistic conformation.
 
 Uses a number of pre-defined Povray objects to simplify scene building
 '''
@@ -16,16 +14,13 @@ __status__ = "Template"
 __version__ = "2016.2"
 
 import math
-import copy
-import numpy as np
-from povray import povray, pdb, load_config
-from vapory import Scene, Sphere, Text, LightSource, Texture, Pigment, Cylinder, Interior, Finish
-
+from povray import povray, pdb
+from vapory import Scene, LightSource
 
 def scene_objects():
     ''' Creates molecule objects and any other pre-calculated data '''
     # Store in the global namespace so the scene() method has access
-    global VIAGRA, RAD_PER_SCENE, FRONT_LIGHT
+    global ATP, RAD_PER_SCENE, FRONT_LIGHT
 
     FRONT_LIGHT = LightSource([0, 5, -29], 'color', [1, 1, 1],
                               'fade_distance', 15, 'fade_power', 2,
@@ -36,25 +31,29 @@ def scene_objects():
     RAD_PER_SCENE = (math.pi / 180) * 3
 
     # Read in a PDB file and construct a molecule
-    VIAGRA = pdb.PDBMolecule('pdb/viagra.pdb', center=False)#, offset=[3, -3, -7])
+    ATP = pdb.PDBMolecule('pdb/ATP_ideal.pdb', center=False)
+    # Move to the center, and raise a little
+    ATP.move_to([0, 6, 0])
+    # Rotate so that the N-tail points downwards a bit
+    ATP.rotate([0, 1, 1], [0, 1.5, -0.7])
+
 
 def scene(step):
     ''' Returns the scene at step number (1 step per frame) '''
-    # Place the original molecule
-    VIAGRA.move_to([0, 6, 0])
-    VIAGRA.rotate([0,1,1], [0, 1, -1.5])
-    VIAGRA.show_label(camera=povray.floor_camera, name=True)
-    
     # Create a new molecule by removing a number of atoms from the original molecule
     # This subset molecule is then positioned with the offset parameter
-    subset = VIAGRA.divide([50, 51, 56, 57, 28, 55, 30, 26, 48, 49], 'subset', offset=[-5, -5, 0])
-    subset.show_label(camera=povray.floor_camera, name=True)
+    phosphate = ATP.divide([0, 1, 2, 3, 32, 7, 31], 'phosphate', offset=[0, -4, 0])
+
+    # Show the names of the atoms by using the 'show_label' method. This method requires
+    # the active camera to aim the text at the 'viewer' to make it readable.
+    ATP.show_label(camera=povray.floor_camera, name=True)
+    phosphate.show_label(camera=povray.floor_camera, name=True)
 
     # Return a 'Scene' object containing -all- objects to render, i.e. the camera,
     # lights and in this case, two molecules with its labels.
     return Scene(povray.floor_camera,
-                 objects=[povray.default_light,
-                          FRONT_LIGHT] + VIAGRA.povray_molecule,
+                 objects=[povray.default_light, FRONT_LIGHT, povray.checkered_ground] +
+                 ATP.povray_molecule + phosphate.povray_molecule,
                  included=['colors.inc'])
 
 if __name__ == '__main__':
