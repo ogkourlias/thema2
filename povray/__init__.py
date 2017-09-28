979bbbb119096ab0003a1813fae60d6090ac2987
Marcel Kempenaar <m.kempenaar@pl.hanze.nl>
2017-09-28 15:42:24 +0200

    Project cleanup and updates

--- povray/__init__.py
+++ povray/__init__.py
@@ -1,6 +1,5 @@
-from povray import config
 import logging
-import sys
+from povray import config
 
 # Default configuration file located in the project root
 DEFAULT_CONFIG = 'default.ini'
@@ -12,9 +11,9 @@ SETTINGS = config.Config(DEFAULT_CONFIG)
 logging.basicConfig(level=logging._nameToLevel[SETTINGS.LogLevel])
 logger = logging.getLogger(__name__)
 
-logger.info('["%s"] - Using config file "%s"', sys._getframe().f_code.co_name, DEFAULT_CONFIG)
+logger.info(' Using config file "%s"', DEFAULT_CONFIG)
 
 
 def load_config(config_file):
-    logger.info('["%s"] - Loading config file "%s"', sys._getframe().f_code.co_name, config_file)
+    logger.info(' Loading config file "%s"', config_file)
     return config.Config(config_file)
