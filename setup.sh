7cde16fe036d8f0a502e635305cd59b3d554b774
Marcel Kempenaar <m.kempenaar@pl.hanze.nl>
2017-11-22 10:35:31 +0100

    Fixed the creation of symlinks, this should give a working version

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git submodule init
 git submodule update
 
 # Set symlinks for easy-import of Vapory library
-ln -s vapory/vapory/*.py vapory/
+cd vapory
+ln -s vapory/*.py .
 
 echo "\nAll done, please don't forget to edit the 'default.ini' file and then run one of the 'template' Python scripts.\n"
