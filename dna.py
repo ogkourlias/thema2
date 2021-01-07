54c81ee6015150ebb30cf397c6fd5c9be6157212
ogkourlias <o.gkourlias@st.hanze.nl>
2021-01-07 16:37:08 +0100

    Added main movie.py file. Changed directory location in default.ini.

--- dna.py
+++ dna.py
@@ -1,44 +1,52 @@
 #!/usr/bin/env python3
 from pypovray import pypovray, models, load_config
 import sys
-from vapory import Cone, Cylinder, Scene, Text, Camera
+from vapory import Cone, Cylinder, Scene, Text, Camera, Merge
 
 SETTINGS = load_config('default.ini')
 sequence = (sys.argv[1])
 pov = Camera('location', [0, 20, -80], 'look_at', [0, 2, -5])
 
-def nucleotide(letter, nucleotide_distance):
-    if letter == "C":
-        nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
-        nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
-    elif letter == "G":
-        nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
-        nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
-    elif letter == "A":
-        nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
-        nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
-    elif letter == "T":
-        nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
-        nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
 
-    return [nucleotidetop, nucleotidebot]
-
-def frame(step):
-    nucleotide1 = []
+def nucleotide(sequence):
     nucleotide_distance = 0
-    for letter in sequence:
-        nucleotide1.append(nucleotide(letter, nucleotide_distance))
-        nucleotide_distance += 9
-
     top = Cylinder([-10, 8, 0], [(len(sequence) * 9), 8, 0], 3, models.default_sphere_model)
     bot = Cylinder([-10, -8, 0], [(len(sequence) * 9), -8, 0], 3, models.default_sphere_model)
+    nucleotide1 = Merge(top, bot)
+    for letter in sequence:
+        if letter == "C":
+            nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
+            nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
+            nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
 
-    nucleotide_final = nucleotide1[0]
-    for item in nucleotide1:
-        nucleotide_final += item
+        elif letter == "G":
+            nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
+            nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
+            nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
+
+        elif letter == "A":
+            nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
+            nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
+            nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
+
+        elif letter == "T":
+            nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
+            nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
+            nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
+        nucleotide_distance += 9
+
+
+    return [nucleotide1]
+
+def frame(step):
     # Return the Scene object for rendering
+    # top = Cylinder([-10, 8, 0], [(len(sequence) * 9), 8, 0], 3, models.default_sphere_model)
+    # bot = Cylinder([-10, -8, 0], [(len(sequence) * 9), -8, 0], 3, models.default_sphere_model)
+
+    nucleotide_final = nucleotide(sequence)
+
     return Scene(pov,
-                 objects=[models.default_light, top, bot] + nucleotide_final)
+                 objects=[models.default_light] + nucleotide_final)
 
 
 if __name__ == '__main__':
--- movie.py
+++ movie.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 import math
 from pypovray import pypovray, SETTINGS, models, pdb, logger
-from vapory import Scene, Plane, Camera
+from vapory import *
 
-
-def molecules():
+from dna import sequence, nucleotide, pov
+from rna_polymerase import rna_polymerase
 
 
 def frame(step):
+
     """ Returns the scene at step number (1 step per frame) """
     # Show some information about how far we are with rendering
     curr_time = step / eval(SETTINGS.NumberFrames) * eval(SETTINGS.FrameTime)
     logger.info(" @Time: %.3fs, Step: %d", curr_time, step)
-
     # Getting the total number of frames, see the configuration file
     nframes = eval(SETTINGS.NumberFrames)
-    rotation = (math.pi / 180) * (360 / nframes)
-    ATP.rotate([0, 1, 0], rotation)
-    phosphate.rotate([0, 1, 0], rotation)
-    # Return the Scene object for rendering
-    print(rotation)
-    return Scene(Camera('location', [0, 18, -20], 'look_at', [0, 0, 0]),
-                 objects=[models.default_light,
-                          models.checkered_ground] + ATP.povray_molecule + phosphate.povray_molecule)
+
+    return Scene(pov,
+                 objects=[models.default_light, polymerase] + nucleotide_final)
 
 
 if __name__ == '__main__':
-    # Create molecule(s)
-    molecules()
+
+    nucleotide_final = nucleotide(sequence)
+    polymerase = rna_polymerase([0, 2, 0], 3)
 
     # Render as a single image
     pypovray.render_scene_to_mp4(frame)
