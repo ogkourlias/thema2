020a9f1e8d080fc2f2cbaacd9bb00ff77cfedd46
ogkourlias <o.gkourlias@st.hanze.nl>
2021-01-11 19:05:30 +0100

    Changed dna and movie.py so the polymerase moves until the end of the animation.

--- dna.py
+++ dna.py
@@ -18,37 +18,31 @@ def nucleotide(sequence):
     bot = Cylinder([-10, -8, 0], [(len(pre_tata) * 9), -8, 0], 3, models.default_dna_model)
     pre_tata_distance = (len(pre_tata)*9)
     nucleotide1 = Merge(top, bot)
-    rna_sequence = ""
 
     for letter in pre_tata:
+
         if letter == "C":
             nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
             nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
             nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
-            rna_sequence += letter
 
         elif letter == "G":
             nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_g_model)
             nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_c_model)
             nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
-            rna_sequence += letter
 
         elif letter == "A":
             nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
             nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
             nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
-            rna_sequence += letter
 
         elif letter == "T":
             nucleotidetop = Cylinder([nucleotide_distance, 6, 0], [nucleotide_distance, 0, 0], 3, models.default_t_model)
             nucleotidebot = Cylinder([nucleotide_distance, -6, 0], [nucleotide_distance, 0, 0], 3, models.default_a_model)
             nucleotide1 = Merge(nucleotide1, nucleotidetop, nucleotidebot)
-            rna_sequence += "U"
 
 
         nucleotide_distance += 9
-
-    print(pre_tata, "followed by", post_tata)
 
     return [nucleotide1, post_tata, pre_tata_distance, nucleotide_distance]
 
@@ -61,8 +55,10 @@ def rna_objects(post_tata, cam_x, nucleotide_distance, stretch_rate = 0):
     default_top = 0
     default_bot = 0
 
-    transition_top = Cylinder([cam_x + 20, stretch_positive + stretch_rate, 0], [(len(post_tata) * 9), stretch_positive + stretch_rate, 0], 3, models.default_dna_model)
-    transition_bot = Cylinder([cam_x + 20, stretch_negative - stretch_rate, 0], [(len(post_tata) * 9), stretch_negative - stretch_rate, 0], 3, models.default_dna_model)
+    transition_top = Cylinder([cam_x + 20, stretch_positive + stretch_rate, 0], [(len(post_tata) * 9)+cam_x , stretch_positive + stretch_rate, 0], 3, models.default_dna_model)
+    transition_bot = Cylinder([cam_x + 20, stretch_negative - stretch_rate, 0], [(len(post_tata) * 9)+cam_x , stretch_negative - stretch_rate, 0], 3, models.default_dna_model)
+
+    post_tata_distance = len(post_tata) * 9
 
     stretch_top = Cylinder([cam_x, stretch_positive, 0], [cam_x + 20, stretch_positive + stretch_rate, 0], 3, models.default_dna_model)
     stretch_bot = Cylinder([cam_x, stretch_negative, 0], [cam_x + 20, stretch_negative - stretch_rate, 0], 3, models.default_t_model)
@@ -94,7 +90,13 @@ def rna_objects(post_tata, cam_x, nucleotide_distance, stretch_rate = 0):
             nucleotide_bot_stretched = Merge(nucleotide_bot_stretched, nucleotidebot)
 
         nucleotide_distance += 9
-    return [transition_top, transition_bot, stretch_top, stretch_bot, nucleotide_top_stretched, nucleotide_bot_stretched]
+    return [transition_top, transition_bot, stretch_top, stretch_bot, nucleotide_top_stretched, nucleotide_bot_stretched, post_tata_distance]
+
+def synthesis(post_tata, cam_x, nucleotide_distance):
+
+    return []
+
+
 
 def frame(step):
     # Return the Scene object for rendering
--- movie.py
+++ movie.py
@@ -10,41 +10,46 @@ from rna_polymerase import rna_polymerase
 
 def frame(step):
 
-    nucleotide_final, rna_sequence, pre_tata_distance, nucleotide_distance = nucleotide(sequence)
+    nucleotide_final, post_tata_sequence, pre_tata_distance, nucleotide_distance = nucleotide(sequence)
 
     """ Returns the scene at step number (1 step per frame) """
     # Show some information about how far we are with rendering
     curr_time = step / eval(SETTINGS.NumberFrames) * eval(SETTINGS.FrameTime)
     logger.info(" @Time: %.3fs, Step: %d", curr_time, step)
-    print(rna_sequence)
     # Getting the total number of frames, see the configuration file
     nframes = eval(SETTINGS.NumberFrames)
     # camera
 
     print(step)
 
-    if step < 80:
+    if step < (0.2*nframes):
         camera_distance = pre_tata_distance
         camera_speed = camera_distance / 80
         cam_x = camera_speed*step
         camera = Camera('location', [cam_x, 40, -80], 'look_at', [camera_speed*step, 0, 0])
         polymerase = rna_polymerase([cam_x, 0, 0], 12)
-        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched = rna_objects(rna_sequence, pre_tata_distance, nucleotide_distance)
-    elif step >= 80 and step < 160:
+        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched, post_tata_distance = rna_objects(post_tata_sequence, pre_tata_distance, nucleotide_distance)
+    elif step >= (0.2*nframes) and step < (0.4*nframes):
         cam_x = pre_tata_distance
         camera = Camera('location', [cam_x, 40, -80], 'look_at', [cam_x, 0, 0])
         polymerase = rna_polymerase([cam_x, 0, 0], 12)
-        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched = rna_objects(rna_sequence, pre_tata_distance, nucleotide_distance)
-    else: #elif step >= 160 and step < 240:
+        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched, post_tata_distance = rna_objects(post_tata_sequence, pre_tata_distance, nucleotide_distance)
+    elif step >= (0.4*nframes) and step < (0.6*nframes):
         cam_x = pre_tata_distance
         polymerase = rna_polymerase([cam_x, 0, 0], 12)
         camera = Camera('location', [cam_x, 40, -80], 'look_at', [cam_x, 0, 0])
-        stretch_speed = ((step - 159) * (12/4))
+        stretch_speed = ((step - 159) * (12/80))
         print(stretch_speed)
-        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched = rna_objects(rna_sequence, pre_tata_distance, nucleotide_distance, stretch_speed)
+        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched, post_tata_distance = rna_objects(post_tata_sequence, pre_tata_distance, nucleotide_distance, stretch_speed)
+    else:
+        stretch_speed = (80 * (12/80))
+        transition_top, transition_bot, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched, post_tata_distance = rna_objects(post_tata_sequence, pre_tata_distance, nucleotide_distance, stretch_speed)
+        post_tata_x = pre_tata_distance + (step - 0.6*nframes) * (post_tata_distance / (nframes*0.4))
+        polymerase = rna_polymerase([post_tata_x, 0, 0], 12)
+        camera = Camera('location', [post_tata_x, 40, -80], 'look_at', [post_tata_x, 0, 0])
+        print(stretch_speed)
 
 
-    # transition_top, transition_bot, stretch_bot, stretch_top = rna_objects(rna_sequence, pre_tata_distance)
 
     return Scene(camera,
                  objects=[models.default_light, polymerase, nucleotide_final, transition_bot, transition_top, stretch_bot, stretch_top, nucleotide_top_stretched, nucleotide_bot_stretched])
